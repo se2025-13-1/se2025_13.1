7b77bded0e1bab96f32bc3b024737df0b230641b
MinhDuc <ducnm160704@gmail.com>
2025-11-28 11:52:25 +0700

    Implement category APIs

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,6 +12,7 @@ import { AuthRepository } from "./modules/auth/auth.repository.js";
 // Import routes
 import authRoutes from "./modules/auth/auth.routes.js";
 import productRoutes from "./modules/product/product.routes.js";
+import categoryRoutes from "./modules/category/category.routes.js";
 
 dotenv.config();
 
@@ -42,6 +43,7 @@ app.get("/", (req, res) => {
 // Module routes
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/categories", categoryRoutes);
 
 // =========================
 // ❗ GLOBAL ERROR HANDLER
